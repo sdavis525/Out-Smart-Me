af19750f1ad53be5903adaf071dfd53ea2ffdeeb
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-03-05 12:14:14 -0500

    quiz generates question

---
+++ assets/js/quiz-time.js
@@ -0,0 +1,88 @@
+const startButton = document.getElementById("startBtn");
+const questions = document.getElementById("questions");
+const questionTitle = document.getElementById("question-title");
+const questionAnswers = document.getElementById("question-answers");
+const startScreen = document.getElementById("start-screen");
+
+//start quiz//
+
+function generateQuiz (){
+startScreen.setAttribute("class", "hide")
+questions.removeAttribute("class")
+
+
+getQuestion()
+}
+
+function getQuestion() {
+let currentQuestion = allQuestions[0] 
+questionTitle.textContent = currentQuestion.question
+questionAnswers.innerHTML = ""
+currentQuestion.answers.forEach(function(choice, i){
+console.log(choice)
+})
+}
+// set timer 90 sec and cout down //
+// lose 10 sec on timer for wrong answer//
+
+//show questions//
+const allQuestions = [
+{
+question: "Inside which Html Element do we put the JS?",
+answers:[
+{ text: "javascript" , correct:false},
+{ text: "script" , correct:true},
+{ text: "js" , correct:false},
+{ text: "jQuery" , correct:false}
+]
+},
+{
+question: "The external JavaScript file must contain the script tag.",
+answers: [
+{ text: "True", correct: false },
+{ text: "False", correct: true }
+]
+},
+{
+question: 'How do you write "Hello World" in an alert box?',
+answers: [
+{ text: 'msg("Hello World");', correct: false },
+{ text: 'prompt("Hello World");', correct: false },
+{ text: 'alertBox("Hello World");', correct: false },
+{ text: 'alert("Hello World");', correct: true }
+]
+},
+{
+question: "How do you create a function in JavaScript?",
+answers: [
+{ text: "function myFunction()", correct: true },
+{ text: "function = myFunction()", correct: false },
+{ text: "make.function.myFunction()", correct: false },
+{ text: "function:myFunction()", correct: false }
+]
+},
+{
+question: 'How do you call a function named "myFunction"?',
+answers: [
+{ text: "call myFunction()", correct: false },
+{ text: "read myFunction()", correct: false },
+{ text: "myFunction()", correct: true },
+{ text: "run.myFunction()", correct: false }
+]
+}
+]
+
+
+// show answers//
+
+// give correct or wrong answer//
+
+// move on once answer is correct //
+
+// end game if timer reaches 0//
+// show high scores//
+//after quiz submit high score//
+startButton.onclick = generateQuiz;
+
+//choice.text (the answer in all questions arr)
+
